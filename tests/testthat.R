2700cc99ff3356fbb8b0346428def0510a7290a0
William Wiik <wilwi856@student.liu.se>
2023-10-16 14:11:28 +0200

    changed testfile

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/reference/test_package.html#special-files
 
 library(testthat)
-library(lab4package)
+library(bonuslabpackage)
 
-test_check("lab4package")
+test_check("bonuslabpackage")
